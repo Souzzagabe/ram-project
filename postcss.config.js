b2ff5177988de07b446676037f2313e591eaa04d
souzzagab <gabriel.souza@lacreisaude.com.br>
2024-05-11 23:24:15 -0300

    feature: first commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Outros plugins do PostCSS, se necessário
+  ]
+}
---
+++ src/components/@types/Types.ts
@@ -0,0 +1,14 @@
+export interface CharacterData {
+    name: string;
+    image: string;
+    status: string;
+    species: string;
+    gender: string;
+  }
+
+  export interface EpisodeData {
+    episode: string;
+    title: string;
+    air_date: string;
+  }
+  
